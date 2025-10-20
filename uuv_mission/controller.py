ac52e4a3f286bea9d860515795abf494983e3400
Diego Danese <diego.danese@outlook.com>
2025-10-20 15:13:02 +0100

    Zaheen changes

---
+++ uuv_mission/controller.py
@@ -0,0 +1,8 @@
+class YourControllerImplementation:
+    def __init__(self):
+        pass
+
+    def compute_control(self, state, reference):
+        # Implement your control logic here
+        control_input = None
+        return control_input
